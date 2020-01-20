d448f631b65ae760398ca92b391fcb619c3b8446
MinexAutomation <minex.automation@gmail.com>
2020-01-19 17:54:54 -0800

    Changed ApplicationBuilder to derive from ServiceProviderBuilder (since this is the actually correct concept).

--- source/R5T.Richmond/Code/Classes/ApplicationBuilder.cs
+++ source/R5T.Richmond/Code/Classes/ApplicationBuilder.cs
@@ -6,11 +6,11 @@ namespace R5T.Richmond
     /// <summary>
     /// Empty class that services as an instantiation for extension methods.
     /// </summary>
-    public class ApplicationBuilder
+    public class ApplicationBuilder : ServiceProviderBuilder
     {
         #region Static
 
-        public static ApplicationBuilder New()
+        public static new ApplicationBuilder New()
         {
             var applicationBuilder = new ApplicationBuilder();
             return applicationBuilder;
