e3d8e4a26458984ccab74f073dc27e88a9170959
MinexAutomation <minex.automation@gmail.com>
2020-01-19 17:53:06 -0800

    Added a ServiceProviderBuilder class since ApplicationBuilder is incorrectly named!

---
+++ source/R5T.Richmond/Code/Classes/ServiceProviderBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace R5T.Richmond
+{
+    public class ServiceProviderBuilder
+    {
+        #region Static
+
+        public static ServiceProviderBuilder New()
+        {
+            var applicationBuilder = new ServiceProviderBuilder();
+            return applicationBuilder;
+        }
+
+        public static IServiceProvider NewUseStartup<TStartup>()
+            where TStartup: class, IStartup
+        {
+            var serviceProvider = ServiceProviderBuilder.New().UseStartup<TStartup>();
+            return serviceProvider;
+        }
+
+        #endregion
+    }
+}
