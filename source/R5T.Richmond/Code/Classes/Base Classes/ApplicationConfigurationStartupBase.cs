e6bebae247dca1e158455ea59152c46d0d8aca36
MinexAutomation <minex.automation@gmail.com>
2020-01-19 17:55:31 -0800

    Follow-through on change to IConfigurationStartup.

--- source/R5T.Richmond/Code/Classes/Base Classes/ApplicationConfigurationStartupBase.cs
+++ source/R5T.Richmond/Code/Classes/Base Classes/ApplicationConfigurationStartupBase.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 
 namespace R5T.Richmond
 {
-    public class ApplicationConfigurationStartupBase : StartupBase, IApplicationConfigurationStartup
+    public class ApplicationConfigurationStartupBase : StartupBase, IConfigurationStartup
     {
         public ApplicationConfigurationStartupBase(ILogger<ApplicationConfigurationStartupBase> logger)
             : base(logger)
