86143ddea3f9d692d5e9f4c80f8a67ef8ce60020
MinexAutomation <minex.automation@gmail.com>
2020-01-19 17:54:01 -0800

    Changed name of IConfigurationStartup type.

--- source/R5T.Richmond/Code/Interfaces/IConfigurationStartup.cs
+++ source/R5T.Richmond/Code/Interfaces/IConfigurationStartup.cs
@@ -7,7 +7,7 @@ namespace R5T.Richmond
     /// Describes the startup for the first-stage of a two-stage dependency-injection container application startup process.
     /// Two-stage application startup is required since a service-provider might be needed during configuration of the configuration builder for the second-stage, actual application startup.
     /// </summary>
-    public interface IApplicationConfigurationStartup : IApplicationStartup
+    public interface IConfigurationStartup : IStartup
     {
     }
 }
